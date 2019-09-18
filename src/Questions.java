bf1cdb61bd761c203729870f69c96481b47bc03f
Albert Zhong <albert.zhong1999@gmail.com>
2019-09-17 22:31:11 -0400

    Push questions and testcases

---
+++ src/Questions.java
@@ -0,0 +1,184 @@
+/*
+* CS 193 Homework 3
+*
+* TODO: READ THESE INSTRUCTIONS!
+*
+* The comments in the methods are for clarification. No tricks or riddles.
+*
+* The overall algorithm should not change. Don't focus on improving the runtime. 
+*
+* Debug in whichever way you feel is best. 
+*
+* Bonus: Understand these methods! They are helpful in interviews :)
+* 
+*/
+
+import java.util.*;
+
+public class Questions {
+
+    public static int findMax(int[] input){
+        // find the max in the input array
+        int max = Integer.MAX_VALUE;
+        for(int i = 0; i<= input.length; i++){
+            if (input[i] > max){
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int[] input){
+        // find the smallest element in the array
+        int min = Integer.MIN_VALUE;
+        for(int i = 0; i<= input.length; i++){
+            if (input[i] > min){
+                min = input[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findSum(int[] input){
+        // find the sum of all the elements in the array
+        int sum = 0;
+        for(int i = 1; i<input.length; i++){
+            sum = sum + input[i];
+        }
+        return sum;
+    }
+
+    public static int findAverage(int[] input){
+        // find the average of the input
+        int sum = 0;
+        for(int i = 1; i< input.length; i++){
+            sum = input[i] - sum;
+        }
+        int average = sum / (input.length -1);
+        return average;
+    }
+
+    public static ArrayList<String> fizzBuzz(int n){ // TODO:MEDIUM
+        // EXPLANATION: RETURN A LIST 1 TO n BUT
+        // MULTIPLES OF 3 ARE REPLACED BY "fizz"
+        // MULTIPLES OF 5 ARE REPLACED BY "buzz"
+        // MULTIPLES OF 3 AND 5 ARE REPLACED BY "fizzbuzz"
+        //
+        // FROM 1 TO 15 THE LIST WOULD LOOK LIKE
+        // {"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz"}
+        // Note: In java an array is different from a list
+        // The data structure we will be using is an ArrayList
+
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(int i = 1; i<= n; i++){
+            if (i % 3 == 1){
+                answer.add("fizz");
+            }else if(i % 5 == 1){
+                answer.add("buzz");
+            }else if(i % 15 == 1){
+                answer.add("fizzbuzz");
+            }else{
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+
+    }
+    // Look for a specific element in sorted array
+    // keep in mind for this algorithm to work, array HAS to be sorted
+    public static int binarySearch(int[] input, int target){ // TODO: (DELETE COMMENT LATER) RANK: MEDIUM
+        // look for target in input
+        int low = 0;
+        int high = input.length -1;
+        while (low < high){
+            int mid = (low + high)/2;
+            if(input[mid] == target){ // middle element is the target. Success!!!
+                return input[mid];
+            }else if(input[mid] > target){ // middle element is greater than the target
+                low = mid+1;
+            }else{ // middle element is smaller than the target
+                high = mid-1;
+            }
+        }
+        return -1; // element is not found
+    }
+
+    public static int countDuplicates(String input){ // TODO: RANK MEDIUM
+        // TODO: count the number of letters that are duplicated
+        // The input string will always be lower case and contain no numbers
+        // Example: Input: This is homework, Return: 3 (letters i, s, and o occur more than once
+
+        input = input.toLowerCase(); // ensuring string is lower case
+        int[] alphabetTemplate = new int[26];
+        for(int i = 0; i<input.length();i++){// iterate over the string
+            int index = input.charAt(i) - 'a'; // Math in ASCII tables.
+            alphabetTemplate[index] += 1;
+        }
+        int counter = 0;
+        for (int i = 0; i<alphabetTemplate.length; i++){
+            if (alphabetTemplate[i] > 0){
+                counter = counter + 1;
+            }
+        }
+        return counter;
+    }
+
+    public static int sumBetween193(int[] input){
+        // TODO: SUM UP NUMBERS BETWEEN THE FIRST 193 AND THE NEXT 193 NON EXCLUSIVE
+        int sum = 0;
+        boolean startCounting = false;
+        for(int i = 0; i< input.length; i++) {
+            if (startCounting) {
+                sum = sum * input[i];
+                if (input[i] == 193){
+                    startCounting = false;
+                }
+            }else{
+                if(input[i] == 193){
+                    startCounting = true;
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static int reverseNumber(int input){ // TODO: RANK HARD
+        //TODO: reverse the number
+        // 12345 should become 54321
+        // Hint: How would you turn 9 into 95? Not by adding 86
+        int answer = 1;
+        while(input != 0){
+            int digit = input % 10;
+            answer = answer + digit;
+            input = input/10;
+        }
+        return answer;
+    }
+
+    public static boolean findSubstring(String theBigOne, String sub){  // TODO: (DELETE COMMENT LATER) RANK: MED/HARD
+        // checks to see if variable sub appears in theBigOne
+        // highly recommended to write this one out on a notebook
+        int counter = 0;
+        for(int i =1; i<theBigOne.length();i++){
+            if(theBigOne.charAt(i) == sub.charAt(0)) {
+                for (int j = 1; j < theBigOne.length(); j++) {
+                    if(theBigOne.charAt(j) == sub.charAt(j-i)){
+                        counter += 1;
+                    }else{ // a character didn't match so break
+                        break;
+                    }
+                    if (counter == sub.length()){
+                        return true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) { // Main method is used for testing purposes. Example given below
+        System.out.println(reverseNumber(54321)); // Run reverseNumber with your own input
+    }
+
+}
---
+++ src/TestCases.java
@@ -0,0 +1,85 @@
+// NOTE: If this import is giving you issues, make sure you
+// hover over it and either click the red lightbulb, or press
+// ALT + ENTER to "Add Junit4 to Classpath". This will allow
+// you to run the test cases!
+
+import org.junit.*;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCases {
+
+    @Test
+    public void testFindMax() {
+        assertEquals(90, Questions.findMax(new int[]{1,2,3,90,83,23}));
+        assertEquals(12345, Questions.findMax(new int[]{999,23,4321,12345,32,1,2,39,2,4,21}));
+        assertEquals(1, Questions.findMax(new int[]{0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0}));
+    }
+
+    @Test
+    public void testFindMin() {
+        assertEquals(1, Questions.findMin(new int[]{1,2,3,4,5,6,7}));
+        assertEquals(Integer.MIN_VALUE, Questions.findMin(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 1,2,3,4}));
+        assertEquals(100, Questions.findMin(new int[]{111111,2333,432432,100,432143}));
+    }
+
+    @Test
+    public void testFindSum() {
+        assertEquals(15, Questions.findSum(new int[]{1,2,3,4,5}));
+        assertEquals(25, Questions.findSum(new int[]{3,5,7,10}));
+        assertEquals(1, Questions.findSum(new int[]{0,1,0,0,0,0,0,0,0}));
+    }
+
+    @Test
+    public void testFindAverage() {
+        assertEquals(3, Questions.findAverage(new int[]{1,2,3,4,5}));
+        assertEquals(5, Questions.findAverage(new int[]{3,5,7,10,0}));
+        assertEquals(10, Questions.findAverage(new int[]{2,4,6,8,10,12,14,16,18}));
+    }
+
+    @Test
+    public void testFizzBuzz() {
+        ArrayList<String> input = new ArrayList<>(Arrays.asList("1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz"));
+        assertEquals(input, Questions.fizzBuzz(15));
+        ArrayList<String> inputAgain = new ArrayList<>(Arrays.asList("1","2","fizz","4","buzz"));
+        assertEquals(inputAgain, Questions.fizzBuzz(5));
+    }
+
+    @Test
+    public void testBinarySearch() {
+        assertEquals(8, Questions.binarySearch(new int[]{1,2,3,4,5,6,7,8}, 8));
+        assertEquals(4, Questions.binarySearch(new int[]{3,4,5,6,7,8,9,10}, 4));
+        assertEquals(10, Questions.binarySearch(new int[]{2,4,6,8,10,12,14,16,18}, 10));
+    }
+
+    @Test
+    public void testCountDuplicates() {
+        assertEquals(3, Questions.countDuplicates("Welcome to cs"));
+        assertEquals(10, Questions.countDuplicates("This is another homework assignment"));
+        assertEquals(3, Questions.countDuplicates("Cereal before milk"));
+    }
+
+    @Test
+    public void testSumBetween193() {
+        assertEquals(5, Questions.sumBetween193(new int[]{1,2,3,4,5,193,1,3,1,193,12}));
+        assertEquals(15, Questions.sumBetween193(new int[]{123,432,198,193,15,193}));
+        assertEquals(0, Questions.sumBetween193(new int[]{193,193,1,2,3}));
+    }
+
+    @Test
+    public void testReverseNumber() {
+        assertEquals(54321, Questions.reverseNumber(12345));
+        assertEquals(12345, Questions.reverseNumber(54321));
+        assertEquals(121, Questions.reverseNumber(121));
+    }
+
+    @Test
+    public void testFindSubstring() {
+        assertEquals(true, Questions.findSubstring("needleinhaystack","needle"));
+        assertEquals(false, Questions.findSubstring("LurkingEverywhere","spy"));
+        assertEquals(true, Questions.findSubstring("ImADuplicate","ImADuplicate"));
+    }
+
+}
